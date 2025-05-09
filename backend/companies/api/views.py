957e10afa5778c47de83af1b057df7c306fa46ab
Fabio Cordoba <fabiocordoba1@gmail.com>
2025-05-08 22:19:06 -0500

    is admin to send email

--- backend/companies/api/views.py
+++ backend/companies/api/views.py
@@ -55,7 +55,7 @@ class ExportProductsPDF(APIView):
         return response
 
 class SendProductsReportEmail(APIView):
-    # permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
 
     def post(self, request):
         email = request.data.get('email')
